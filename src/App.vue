136f8e6983c81fd2aa72ef8d7eec7f99b0bc1270
wwhtml <1129942397@qq.com>
2023-11-12 22:40:32 +0800

    提交

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+// 注意：script标签内容不能为空，如果这里的内容是空就不会编译
+</script>
+
+<template>
+  <div>
+    <el-icon><ChatSquare /></el-icon>
+    <el-icon><ChatLineRound /></el-icon>
+    <el-icon><ChatDotSquare /></el-icon>
+    <el-icon><RefreshRight /></el-icon>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
